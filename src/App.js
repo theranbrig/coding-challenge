f9a66aae63c1442711ee98783e3c768742b7c843
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-10-07 21:08:24 +0900

    removed api key

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Navbar from './components/Navbar';
 import * as firebase from 'firebase';
 
 const config = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	 authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	 databaseURL: process.env.REACT_APP_DATABASE_URL,
-	projectId: process.env.REACT_APP_PROJECT_ID
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	 messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
