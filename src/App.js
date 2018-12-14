d33ea03feaf534a830d248512b71e4335e1223c5
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-12-14 12:02:37 +0900

    updated variables

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Messages from './components/Messages';
 import Navbar from './components/Navbar';
 import * as firebase from 'firebase';
 
+require('dotenv').config();
+
 const config = {
 	apiKey: process.env.REACT_APP_API_KEY,
-	 authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	 databaseURL: process.env.REACT_APP_DATABASE_URL,
-	projectId: process.env.REACT_APP_PROJECT_ID
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
 	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	 messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	 authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	 databaseURL: process.env.REACT_APP_DATABASE_URL,
-	projectId: process.env.REACT_APP_PROJECT_ID
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	 messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+	apiKey: 'AIzaSyANRwiqwbNnLT-2chBMikZ8g64febUJ6VI',
+	authDomain: 'bored-718b1.firebaseapp.com',
+	databaseURL: 'https://bored-718b1.firebaseio.com',
+	projectId: 'bored-718b1',
+	storageBucket: 'bored-718b1.appspot.com',
+	messagingSenderId: '1073064848250'
 };
 
 export default (!firebase.apps.length ? firebase.initializeApp(config) : firebase.app());
